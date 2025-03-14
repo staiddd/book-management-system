7c413a69d36e3c8dbd12baa9eba7af94f8df9998
Kirill <pavlenkokirill120@gmail.com>
2025-03-14 11:04:33 +0200

    adjustment

--- src/config.py
+++ src/config.py
@@ -1,10 +1,18 @@
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-BASE_DIR = Path(__file__).parent
 
-private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
-public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
+def read_key_file(filename: str) -> str:
+    """Reads the contents of the key file, checking two possible directories."""
+    base_dirs = [Path(__file__).parent, Path(__file__).parent.parent]
+
+    for base_dir in base_dirs:
+        key_path = base_dir / "certs" / filename
+        if key_path.exists():
+            return key_path.read_text()
+
+    raise FileNotFoundError(f"Key file '{filename}' not found in expected directories.")
+
 
 class Settings(BaseSettings):
     POSTGRES_USER: str
@@ -15,13 +23,14 @@ class Settings(BaseSettings):
 
     DB_URL: str
 
-    private_key: str = private_key_path.read_text()
-    public_key: str = public_key_path.read_text()
+    private_key: str = read_key_file("jwt-private.pem")
+    public_key: str = read_key_file("jwt-public.pem")
     algorithm: str = "RS256"
 
     access_token_expire_minutes: int = 15
     refresh_token_expire_days: int = 30
 
-    model_config = SettingsConfigDict(env_file='.env', extra="ignore")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
 
 settings = Settings()
